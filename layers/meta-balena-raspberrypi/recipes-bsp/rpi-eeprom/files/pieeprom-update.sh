437510271110d1ec7169be002fff5496cf9f2840
Alex Gonzalez <alexg@balena.io>
2023-11-20 12:03:09 +0100

    rpi-eeprom: remove module loading/unloading
    Loading modules on demand is problematic in the case of rollbacks, as
    the running kernel does not match the rolled back root filesystem
    modules.
    
    Replace with building in the SPI support and binding/unbinding the
    driver when the specific dynamic configuration is changed.
    
    Changelog-entry: use built-in SPI drivers for EEPROM access
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- layers/meta-balena-raspberrypi/recipes-bsp/rpi-eeprom/files/pieeprom-update.sh
+++ layers/meta-balena-raspberrypi/recipes-bsp/rpi-eeprom/files/pieeprom-update.sh
@@ -11,50 +11,59 @@ CURR_IMG_PATH=/dev/shm
 NEW_IMG_PATH=/mnt/boot
 SPI_SPEED=16000
 
-function spi_modules() {
-    if [ "load" = $1 ]; then
-        modprobe spidev
-        modprobe spi-bcm2835
-    elif [ "unload" = $1 ]; then
-        rmmod spidev
-        rmmod spi-bcm2835
-    else
-        warn "spi_modules: no arugment provided"
-    fi
+spi_bind() {
+    command="${1}"
+    case $command in
+        "unbind")
+            SPI_BCM2835_SYSPATH="/sys/bus/platform/drivers/spi-bcm2835"
+            SPI_DEVICE="$(basename "$(find "${SPI_BCM2835_SYSPATH}" -type l -name "*.spi" | head -n1)")"
+            if [ -n "${SPI_DEVICE}" ]; then
+                echo "${SPI_DEVICE}" > "${SPI_BCM2835_SYSPATH}/${command}" || true
+            fi
+            ;;
+        "bind")
+            if [ -n "${SPI_DEVICE}" ]; then
+                echo "${SPI_DEVICE}" > "${SPI_BCM2835_SYSPATH}/${command}" || true
+                SPI_DEVICE=""
+            fi
+            ;;
+        *) fatal "$command not supported";;
+    esac
 }
 
 # According to documentation, custom EEPROM update scripts
 # must also check for FREEZE_VERSION flag
-if [ $(/usr/bin/vcgencmd bootloader_config | grep "FREEZE_VERSION=1" || true) ]; then
+if [ "$(/usr/bin/vcgencmd bootloader_config | grep "FREEZE_VERSION=1" || true)" ]; then
     warn "Bootloader config contains FREEZE_VERSION=1. Will NOT update SPI EEPROM firmware!"
     exit 0
 fi
 
-spi_modules unload
+spi_bind unbind
 
 /usr/bin/vcmailbox 0x00030056 4 4 0 > /dev/null || true
 /usr/bin/dtparam -d /mnt/boot/overlays/ audio=off
 /usr/bin/dtoverlay -d /mnt/boot/overlays/ spi-gpio40-45
 
-spi_modules load
+spi_bind bind
 
 /usr/sbin/flashrom -p "linux_spi:dev=/dev/spidev0.0,spispeed=${SPI_SPEED}" --read ${CURR_IMG_PATH}/${CURR_IMG}
 
-curr_eeprom_md5sum=$(md5sum ${CURR_IMG_PATH}/${CURR_IMG} | awk '{print $1}')
-new_eeprom_md5sum=$(md5sum ${NEW_IMG_PATH}/${NEW_IMG} | awk '{print $1}')
+curr_eeprom_md5sum=$(md5sum "${CURR_IMG_PATH}/${CURR_IMG}" | awk '{print $1}')
+new_eeprom_md5sum=$(md5sum "${NEW_IMG_PATH}/${NEW_IMG}" | awk '{print $1}')
 
-if [ $curr_eeprom_md5sum = $new_eeprom_md5sum ]; then
+if [ "$curr_eeprom_md5sum" = "$new_eeprom_md5sum" ]; then
     info "SPI EEPROM fw update is not necessary"
 else
     info "Performing SPI EEPROM fw update..."
-    /usr/sbin/flashrom -p "linux_spi:dev=/dev/spidev0.0,spispeed=${SPI_SPEED}" --write ${NEW_IMG_PATH}/${NEW_IMG}
+    /usr/sbin/flashrom -p "linux_spi:dev=/dev/spidev0.0,spispeed=${SPI_SPEED}" --write "${NEW_IMG_PATH}/${NEW_IMG}"
 fi
 
-spi_modules unload
 rm ${CURR_IMG_PATH}/$CURR_IMG
+
+spi_bind unbind
 
 /usr/bin/dtparam -d /mnt/boot/overlays/ -R spi-gpio40-45
 /usr/bin/dtparam -d /mnt/boot/overlays/ audio=on
 /usr/bin/vcmailbox 0x00030056 4 4 1 > /dev/null || true
 
-spi_modules load
+spi_bind bind
